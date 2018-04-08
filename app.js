18541eb5bb728159481ca4cf04e1734ceedd0115
Arunkumar Maniam Rajan <pragmaticarun@gmail.com>
2018-04-08 23:39:57 +0530

    body parser

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var routes = require('./routes');
-
+var bodyParser = require('body-parser');
 app.set('port', 3000);
 
 app.use(express.static(path.join(__dirname,'public')));
-
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/api',routes)
 
 var server = app.listen(app.get('port'), function() {
